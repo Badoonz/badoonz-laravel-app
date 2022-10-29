1e10fe89c800622f994f031c819ea7c6ddc2aab6
Badoonz <malitskyi.bohdan@student.uzhnu.edu.ua>
2022-10-29 23:43:55 +0300

    Commit for Lab2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::simplepaginate(5);
+        $users = User::paginate(10);
         return view('users.index', ['users' => $users]);
     }
 
