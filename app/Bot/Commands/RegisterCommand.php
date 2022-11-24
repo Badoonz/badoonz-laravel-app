a8b3008f82f94d4a4d90453b53a57487625d2f7b
Badoonz <malitskyi.bohdan@student.uzhnu.edu.ua>
2022-11-24 19:10:16 +0200

    One More Time for Everyone

--- app/Bot/Commands/RegisterCommand.php
+++ app/Bot/Commands/RegisterCommand.php
@@ -2,53 +2,46 @@
 
 namespace App\Bot\Commands;
 
+use App\Models\Chat;
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
 
 class RegisterCommand extends Command
 {
-    /**
-     * @var string Command Name
-     */
+
     protected $name = "register";
 
-    /**
-     * @var string Command Description
-     */
     protected $description = "Register Command to get you started";
 
-    /**
-     * @inheritdoc
-     */
+
     public function handle()
     {
-        // This will send a message using `sendMessage` method behind the scenes to
-        // the user/chat id who triggered this command.
-        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
-        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-        $this->replyWithMessage(['text' => 'Hello! You Are Registered:']);
+        $telegramUpdate = $this->getUpdate();
+        $telegramChat = $telegramUpdate->getChat();
+        $telegramUser = $telegramUpdate->getMessage()->from;
 
-        // This will update the chat status to typing...
-        $this->replyWithChatAction(['action' => Actions::TYPING]);
+        try {
+            $chat = Chat::query()
+                ->where('chat_id', '=', $telegramChat->id)
+                ->get()
+                ->first();
 
-        // This will prepare a list of available commands and send the user.
-        // First, Get an array of all registered commands
-        // They'll be in 'command-name' => 'Command Handler Class' format.
-        $commands = $this->getTelegram()->getCommands();
+            if(!$chat) {
+                $chat = $this->randomCoffee->registerChat(
+                    $telegramChat->id,
+                    "Chat {$telegramChat->id}"
+                );
+            }
 
-        // Build the list
-        $response = '';
-        foreach ($commands as $name => $command) {
-            $response .= sprintf('/%s - %s' . PHP_EOL, $name, $command->getDescription());
+            $this->randomCoffee->registerMember(
+                $telegramUser->id,
+                "{$telegramUser->firstName} {$telegramUser->lastName}",
+                $chat
+            );
+
+            $this->replyWithMessage(['text' => 'Done']);
+        } catch (\Exception $exception) {
+            $this->replyWithMessage(['text' => "Oops... Something went wrong. {$exception->getMessage()}"]);
         }
 
-        // Reply with the commands list
-        $this->replyWithMessage(['text' => $response]);
-
-        // Trigger another command dynamically from within this command
-        // When you want to chain multiple commands within one or process the request further.
-        // The method supports second parameter arguments which you can optionally pass, By default
-        // it'll pass the same arguments that are received for this command originally.
-        $this->triggerCommand('subscribe');
-    }
-}
+}}
